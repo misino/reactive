3d733d4780f7e450f01849ee61d322d8a5854c07
Michal Biros <michal.biros@itera.no>
2016-10-26 11:58:13 +0200

    Task 2: show number of messages in the header (with mobx)

--- src/modules/Index.jsx
+++ src/modules/Index.jsx
@@ -1,35 +1,48 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link, locationShape } from 'react-router';
+import { observer, inject } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 import classnames from 'classnames';
 
-const Index = props => (
-  <div id="Index">
-    {process.env.NODE_ENV !== 'production' && <DevTools />}
-    <nav className="navbar navbar-fixed-top navbar-inverse">
-      <div className="navbar-header">
-        <Link to="/" className="navbar-brand">
-          Reactive
-        </Link>
-      </div>
-      <ul className="nav navbar-nav">
-        <li className={classnames({ active: props.location.pathname.includes('/reduxchat') })}>
-          <Link to="/reduxchat">Redux Chat</Link>
-        </li>
-        <li className={classnames({ active: props.location.pathname.includes('/mobxchat') })}>
-          <Link to="/mobxchat">Mobx Chat</Link>
-        </li>
-      </ul>
-    </nav>
-    <div id="App" className="container-fluid">
-      {props.children}
-    </div>
-  </div>
-);
+import ChatStore from '../mobx/modules/ChatStore';
 
-Index.propTypes = {
-  children: PropTypes.node.isRequired,
-  location: locationShape.isRequired,
-};
+@inject('chat')
+@observer
+class Index extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    location: locationShape.isRequired,
+    // inject
+    chat: PropTypes.instanceOf(ChatStore).isRequired,
+  };
+
+  render() {
+    const { chat } = this.props;
+    return (
+      <div id="Index">
+        {process.env.NODE_ENV !== 'production' && <DevTools />}
+        <nav className="navbar navbar-fixed-top navbar-inverse">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">
+              Reactive
+            </Link>
+          </div>
+          <ul className="nav navbar-nav">
+            <li className={classnames({ active: this.props.location.pathname.includes('/reduxchat') })}>
+              <Link to="/reduxchat">Redux Chat</Link>
+            </li>
+            <li className={classnames({ active: this.props.location.pathname.includes('/mobxchat') })}>
+              <Link to="/mobxchat">Mobx Chat</Link>
+            </li>
+            <li><a>Messages: {chat.messages.size}</a></li>
+          </ul>
+        </nav>
+        <div id="App" className="container-fluid">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Index;
